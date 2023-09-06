5e4b1a34902a6b97213267ee744f34fa097d3ec9
Sebatora <sebastiana.toranzo@gmail.com>
2023-09-06 20:13:07 -0300

    update quiz component

--- src/components/Quiz/Quiz.jsx
+++ src/components/Quiz/Quiz.jsx
@@ -5,7 +5,7 @@ import "./Quiz.scss";
 function Quiz() {
   const { id } = useParams();
   const navigate = useNavigate();
-  
+
   const [isLoading, setIsLoading] = useState(true);
 
   const [questions, setQuestions] = useState([]);
@@ -20,34 +20,34 @@ function Quiz() {
     const data = await response.json();
     setQuestions(data);
     setIsLoading(false);
-  }
+  };
 
   const handleExit = () => {
     setPoints(0);
-    setCurrentQuestion(0)
+    setCurrentQuestion(0);
     navigate("/");
-  }
+  };
 
   const handleNext = () => {
-    setCurrentQuestion(currentQuestion+1)
+    setIsAnswered(false);
+    setCurrentQuestion(currentQuestion + 1);
     setOptions(shuffleOptions());
-  }
+    setAnswerIndex(null)
+  };
 
   const handleEnd = () => {
-    alert(`Sumaste ${points} puntos`)
+    alert(`Sumaste ${points} ${points === 1 ? `punto` : `puntos`}`);
     setPoints(0);
-    setCurrentQuestion(0)
+    setCurrentQuestion(0);
     navigate("/");
-  }
+  };
 
   const handleAnswer = (selectedAnswer, index) => {
+    if (selectedAnswer.isCorrect) setPoints(points + 1);
+    setIsAnswered(true);
+    setAnswerIndex(index)
+  };
 
-    if (selectedAnswer.isCorrect){
-      setPoints(points+1)}
-
-      setIsAnswered(true)
-    }
-  
   useEffect(() => {
     fetchData();
   }, []);
@@ -58,23 +58,23 @@ function Quiz() {
 
   const shuffleOptions = () => {
     return [
-      {option: questions[currentQuestion].option1, isCorrect:true},
-      {option: questions[currentQuestion].option2, isCorrect:false},
-      {option: questions[currentQuestion].option3, isCorrect:false},
-      {option: questions[currentQuestion].option4, isCorrect:false}
+      { option: questions[currentQuestion].option1, isCorrect: true },
+      { option: questions[currentQuestion].option2, isCorrect: false },
+      { option: questions[currentQuestion].option3, isCorrect: false },
+      { option: questions[currentQuestion].option4, isCorrect: false },
     ].sort(() => Math.random() - 0.5);
-  }
+  };
 
   return (
     <div className="quiz-container">
       <section className="quiz-top-bar">
-        <button id="quiz-exit" onClick={handleExit}>X</button>
+        <button id="quiz-exit" onClick={handleExit}>
+          X
+        </button>
         <div id="quiz-progress-bar">
           <div id="quiz-progress-bar-full" />
           <span>
-            {
-              !isLoading && (`${currentQuestion+1}/${questions.length}`)
-            }
+            {!isLoading && `${currentQuestion + 1}/${questions.length}`}
           </span>
         </div>
       </section>
@@ -82,38 +82,41 @@ function Quiz() {
       <section className="quiz-question">
         <div className="quiz-timer">TIMER</div>
         <div className="quiz-question-text">
-          {
-            !isLoading && questions[currentQuestion].question
-          }
+          {!isLoading && questions[currentQuestion].question}
         </div>
       </section>
 
       <section className="quiz-answers">
-        {
-          !isLoading && (
-            <div>
-              {options.map((option, index) => (
-                <button onClick={() => handleAnswer(option, index)} key={index} value={index} className="quiz-option">
-                  <p>{option.option}</p>
-                  <div className="quiz-option-check" />
-                </button>
-              ))}
-            </div>
-          )
-        }
+        {!isLoading && (
+          <div>
+            {options.map((option, index) => (
+              <button
+                onClick={() => handleAnswer(option, index)}
+                key={index}
+                value={index}
+                disabled={isAnswered && answerIndex !== index}
+                className={`quiz-option ${answerIndex !== null && index === answerIndex ? option.isCorrect === true ? "correct" : `incorrect ${option.isCorrect && `correct-option`}` : "" }`}
+              >
+                <p className="quiz-option-text">{option.option}</p>
+                <div className="quiz-option-check">
+                  {
+                    answerIndex !== null && index === answerIndex ? option.isCorrect === true ? "✔️" : "✖️" : ""
+                  }
+                </div>
+              </button>
+            ))}
+          </div>
+        )}
       </section>
 
-      {
-        !isLoading && (
-          currentQuestion + 1 === questions.length ? (
-            <button onClick={handleEnd}>End</button>
-            ) : (
-            <button onClick={handleNext}>Next</button>
-          )
-        )
-      }
+      {!isLoading &&
+        (currentQuestion + 1 !== questions.length ? (
+          <button onClick={handleNext} disabled={!isAnswered} >Next</button>
+          ) : (
+            <button onClick={handleEnd} disabled={!isAnswered}>End</button>
+          ))}
     </div>
-  )
+  );
 }
 
 export default Quiz;
